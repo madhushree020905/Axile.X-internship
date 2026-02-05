646ec06bf7b6590974c9ddc9aa4bf71da17afec1
madhushree292005 <madhusri29005@gmail.com>
2026-02-05 20:47:38 +0530

    Day 1 internship tasks

---
+++ day-1/arithmetic operations/arithmetic operations.py
@@ -0,0 +1,12 @@
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+
+addition=a+b
+subtraction=a-b
+multiply=a*b
+division=a/b
+
+print("Addition =", addition)
+print("Subtraction =", subtraction)
+print("Multiplication =", multiply)
+print("Division =", division)
